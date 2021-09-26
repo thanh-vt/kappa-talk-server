08af0c1e134f749a8c417879d0477800081037ca
thanhvt <thanhvt@vissoft.vn>
2021-09-26 18:56:57 +0700

    update SSLConfig

--- src/main/java/com/kappa/config/security/SSLConfig.java
+++ src/main/java/com/kappa/config/security/SSLConfig.java
@@ -7,10 +7,12 @@ import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 import lombok.extern.log4j.Log4j2;
 import org.apache.http.client.HttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -81,6 +83,23 @@ public class SSLConfig {
 //        System.setProperty("javax.net.ssl.trustStorePassword", Objects.requireNonNull(trustStorePassword));
 //        System.setProperty("javax.net.ssl.trustStoreType", Objects.requireNonNull(trustStoreType));
 //    }
+
+    private TrustManager[] createIgnoreAllTrm() {
+        TrustManager trm = new X509TrustManager() {
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                // do not check
+            }
+
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                // do not check
+            }
+        };
+        return new TrustManager[] {trm};
+    }
 
     //    @PostConstruct
     public SSLContext customSSL() {
